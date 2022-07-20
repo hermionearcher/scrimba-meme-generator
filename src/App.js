4098ee68e06e6147d355d185321b7de92a611b7b
hermionearcher <46831152+hermionearcher@users.noreply.github.com>
2022-07-20 11:29:33 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Generate from './components/Generate';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Generate/>
+    </div>
+  );
+}
+
+export default App;
