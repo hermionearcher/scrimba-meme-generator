33241f7555feb04a6e4e34a6c6552c5c52b91256
hermionearcher <hermione-archer@hotmail.co.uk>
2022-07-20 11:34:48 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Generate from './components/Generate';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Generate/>
+      <Header />
+      <Generate />
     </div>
   );
 }
